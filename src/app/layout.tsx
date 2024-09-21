20acfcbcf215a2609e4ec6bb99665f38e5bce53c
saugat23 <saugatbhandari8@gmail.com>
2024-09-21 09:55:43 +0545

    smooth scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.variable}`}
+        className={`${inter.variable} scroll-smooth`}
       >
         <SmoothScrolling>{children}</SmoothScrolling>
       </body>
